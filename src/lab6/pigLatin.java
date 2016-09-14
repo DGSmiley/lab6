eb44738b78d10cc7819a5e29f931f6eb524af00d
David Smiley <DGSmiley77@gmail.com>
2016-09-14 11:46:01 -0400

    printout in lowercase

--- src/lab6/pigLatin.java
+++ src/lab6/pigLatin.java
@@ -6,10 +6,12 @@ public class pigLatin {
 
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		// header
+		// header and input option
 		System.out.println("Welcome to the Pig Latin Translator!");
 		System.out.print("Enter a line to be translated: ");
 		String sentence = input.nextLine();
+		
+		System.out.println(sentence.toLowerCase());
 
 	}
 
